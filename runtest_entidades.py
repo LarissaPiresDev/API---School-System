e4b58a121338c20ebd2c08c96441b5fdad5adaad
Wender da Silva Santos <wenderman74@gmail.com>
2025-03-17 17:19:28 -0300

    test: adicionando mais 3 testes para turma onde cada um respectivamente verificam se a nossa aplicação ao ler separadamente os dados da turma: se o id passado é inteiro, se ela verifica se o id é maior ou igual a 0, e caso o id informado seja inexistente

--- runtest_entidades.py
+++ runtest_entidades.py
@@ -53,7 +53,26 @@ class TestStringMethods(unittest.TestCase):
         except:
             self.fail("queria um json mas voce retornou outra coisa")
 
-        self.assertEqual(type(obj_retornado),type([]))  
+        self.assertEqual(type(obj_retornado),type([]))
+
+    def test_002_id_de_turma_não_int(self):
+        resposta = requests.get('http://localhost:5002/turmas/1.5')
+        if resposta.status_code == 400:
+            resp_retornada = resposta.json()
+            assert {'mensagem': 'ID inserido para turma precisa ser um numero inteiro'} == resp_retornada
+
+    def test_003_id_de_turma_menor_igual_que_zero(self):
+        resposta = requests.get('http://localhost:5002/turmas/-1')
+        if resposta.status_code == 400:
+            resp_retornada = resposta.json()                
+            assert {'mensagem': 'ID de turma nao pode ser menor ou igual a que zero'} == resp_retornada
+
+
+    def test_004_id_de_turma_inexistente(self):
+        resposta = requests.get('http://localhost:5002/turmas/500')
+        if resposta.status_code == 404:
+            resp_retornada = resposta.json()
+            assert {'mensagem': 'Turma nao encontrada/inexistente'} == resp_retornada  
 
 
 # -----------------------------------------------ALUNOS---------------------------------------------- #
