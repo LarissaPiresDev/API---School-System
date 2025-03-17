a86773cb0eddd97ed15e90485f75e5a0a9dd2778
Wender da Silva Santos <wenderman74@gmail.com>
2025-03-17 16:50:09 -0300

    feat: adicionando a rota que retorna uma lista de todos os alunos da nossa aplicação

--- app.py
+++ app.py
@@ -4,13 +4,23 @@ import users
 app = Flask(__name__)
 dados = users.users
 
+#--------------------------------------------PROFESSORES------------------------------------------- #
+
 @app.route('/professores/', methods=['GET'])
 @app.route('/professores', methods=['GET'])
 def get_professores():
     professores = dados['Professores']
     return jsonify(professores)
 
+# ---------------------------------------------TURMAS------------------------------------------------ #
 
+@app.route('/turmas/', methods=['GET'])
+@app.route('/turmas', methods=['GET'])
+def get_turmas():
+    turmas = dados['Turmas']
+    return jsonify(turmas) 
+
+# -----------------------------------------------ALUNOS---------------------------------------------- #
 @app.route('/alunos/', methods=['GET'])
 @app.route('/alunos', methods=['GET'])
 def get_alunos():
@@ -20,6 +30,3 @@ def get_alunos():
 
 if __name__ == '__main__':
     app.run(host='localhost', port = 5002,debug=True)
-
-
-
