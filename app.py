91c6fefc7923319f10ca099976c3e0a2d1e70d49
Wender da Silva Santos <wenderman74@gmail.com>
2025-03-17 17:31:46 -0300

    feat: adicionando funcionalidade que lê aluno individualmente de acordo com o id informado na, e trata os erros de acordo com os testes feitos no commit anterior, caso o id: não for um valor de número inteiro, caso o número seje menor ou igual a zero,e caso não id não esteje nos nossos dados

--- app.py
+++ app.py
@@ -61,6 +61,24 @@ def get_alunos():
     alunos = dados['Alunos']
     return jsonify(alunos)
 
+@app.route('/alunos/<id>', methods=['GET'])
+def alunoPorId(id):
+    try:
+        id = int(id)
+    except ValueError:
+        return jsonify({'mensagem': 'ID inserido para aluno tem que ser um numero inteiro'}), 400
+    
+    if id <=0:
+        return jsonify({'mensagem': 'ID do aluno nao pode ser menor ou igual a que zero'}), 400
+    
+    alunos = dados['Alunos']
+    for aluno in alunos:
+        if aluno['id'] == id:
+            return jsonify(aluno)
+    
+    
+    return jsonify({'mensagem': 'Aluno(a) nao encontrado(a)/inexistente'}), 404
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port = 5002,debug=True)
