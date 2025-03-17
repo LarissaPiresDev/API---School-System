a91d9885efd523179c2e4180613f7a64e43f1bb2
Wender da Silva Santos <wenderman74@gmail.com>
2025-03-17 17:05:06 -0300

    feat: adicionando rota para listar professor individualmente, verifica se o valor passado é um numero inteiro, se é menor ou igual a zero, ou se ele encontrou o id informado

--- app.py
+++ app.py
@@ -12,13 +12,31 @@ def get_professores():
     professores = dados['Professores']
     return jsonify(professores)
 
+def profPorId(id):
+    try:
+        id = int(id)
+    except ValueError:
+        return jsonify({'mensagem': 'ID inserido para professor precisa ser um numero inteiro'}), 400
+    
+    if id <= 0:
+        return jsonify({'mensagem': 'ID do professor nao pode ser menor ou igual a que zero'}), 400
+    
+    professores = dados['Professores']
+    for professor in professores:
+        if professor['id'] == id:
+            return jsonify(professor)
+            
+    return jsonify({'mensagem': 'Professor(a) nao encontrado(a)/inexistente'}), 404
+
 # ---------------------------------------------TURMAS------------------------------------------------ #
 
 @app.route('/turmas/', methods=['GET'])
 @app.route('/turmas', methods=['GET'])
 def get_turmas():
     turmas = dados['Turmas']
-    return jsonify(turmas) 
+    return jsonify(turmas)
+
+
 
 # -----------------------------------------------ALUNOS---------------------------------------------- #
 @app.route('/alunos/', methods=['GET'])
