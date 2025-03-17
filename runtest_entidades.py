b05b6d2fa27bd69e1e9503c4251b0cf923656019
Wender da Silva Santos <wenderman74@gmail.com>
2025-03-17 16:21:41 -0300

    test: adicionando arquivo de test com nosso primeiro test

---
+++ runtest_entidades.py
@@ -0,0 +1,24 @@
+import requests
+import unittest
+
+
+class TestStringMethods(unittest.TestCase):
+#--------------------------------------------PROFESSORES------------------------------------------- #
+
+    def test_001_professores_retorna_lista(self):
+        resposta = requests.get('http://localhost:5002/professores')
+        
+        if resposta.status_code == 404:
+            self.fail("voce nao definiu a pagina /professores no seu server")
+
+        try:
+            obj_retornado = resposta.json()
+        
+        except:
+            self.fail("queria um json mas voce retornou outra coisa")
+
+        self.assertEqual(type(obj_retornado),type([]))
+
+
+
+
