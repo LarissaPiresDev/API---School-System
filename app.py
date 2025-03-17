5e07f0f7af0edf0fd108c8970d5a56d655ae12c1
Wender da Silva Santos <wenderman74@gmail.com>
2025-03-17 16:37:40 -0300

    feat: adicionando a rota para realizar a listagem de todos os alunos

--- app.py
+++ app.py
@@ -10,6 +10,16 @@ def get_professores():
     professores = dados['Professores']
     return jsonify(professores)
 
+
+@app.route('/alunos/', methods=['GET'])
+@app.route('/alunos', methods=['GET'])
+def get_alunos():
+    alunos = dados['Alunos']
+    return jsonify(alunos)
+
+
 if __name__ == '__main__':
     app.run(host='localhost', port = 5002,debug=True)
-    
+
+
+
