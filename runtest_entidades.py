b17d3749eea90e0db9e8f29b2b2cad801b99ef75
Wender da Silva Santos <wenderman74@gmail.com>
2025-03-18 12:43:03 -0300

    :test_tube: test: Adicionando testes para metodo post de professor (verificam se nome materia e idade foram informados, pois serão obrigatórios)

--- runtest_entidades.py
+++ runtest_entidades.py
@@ -6,7 +6,7 @@ class TestStringMethods(unittest.TestCase):
 #--------------------------------------------PROFESSORES------------------------------------------- #
 
     def test_001_professores_retorna_lista(self):
-        resposta = requests.get('http://localhost:5002/professores')
+        resposta = requests.get('http://localhost:5003/professores')
         
         if resposta.status_code == 404:
             self.fail("voce nao definiu a pagina /professores no seu server")
@@ -20,28 +20,53 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(type(obj_retornado),type([]))
 
     def test_002_id_de_professor_não_int(self):
-        resposta = requests.get('http://localhost:5002/professores/1.5')
+        resposta = requests.get('http://localhost:5003/professores/1.5')
         
         if resposta.status_code == 400:
             resp_retornada = resposta.json()
             assert {'mensagem': 'ID inserido para professor precisa ser um numero inteiro'} == resp_retornada
     
     def test_003_id_de_professor_menor_igual_que_zero(self):
-        resposta = requests.get('http://localhost:5002/professores/-1')
+        resposta = requests.get('http://localhost:5003/professores/-1')
         if resposta.status_code == 400:
             resp_retornada = resposta.json()                
             assert {'mensagem': 'ID do professor nao pode ser menor ou igual a que zero'} == resp_retornada
 
     def test_004_id_de_professor_inexistente(self):
-        resposta = requests.get('http://localhost:5002/professores/500')
+        resposta = requests.get('http://localhost:5003/professores/500')
         if resposta.status_code == 404:
             resp_retornada = resposta.json()                
             assert {'mensagem': 'Professor(a) nao encontrado(a)/inexistente'} == resp_retornada
 
+    def test_005_se_nome_materia_foram_inseridos(self):
+        novo_professor = {
+            "nome": "",
+            "idade": 45,
+            "materia": "Fisica",
+            "salario": 1500.00
+        }
+
+        resposta = requests.post('http://localhost:5003/professores', json=novo_professor)
+        if not novo_professor['nome'] or not novo_professor['materia']:
+            resp_retornada = resposta.json()
+            assert {'mensagem': 'Para criar um novo professor preciso que voce me passe os parâmetros nome e materia'} == resp_retornada    
+
+    def test_006_se_idade_nao_for_inserida(self):
+        novo_professor = {
+            "nome": "Simas",
+            "materia": "Fisica",
+            "salario": 1500.00
+        }
+
+        resposta = requests.post('http://localhost:5003/professores', json=novo_professor)
+        if 'id' not in novo_professor:
+            resp_retornada = resposta.json()
+            assert {'mensagem': 'Idade e obrigatoria, por favor insira-a'} == resp_retornada
+
 # ---------------------------------------------TURMAS------------------------------------------------ #
 
     def test_001_turmas_retorna_lista(self):
-        resposta = requests.get('http://localhost:5002/turmas')
+        resposta = requests.get('http://localhost:5003/turmas')
 
         
         if resposta.status_code == 404:
@@ -56,28 +81,29 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(type(obj_retornado),type([]))
 
     def test_002_id_de_turma_não_int(self):
-        resposta = requests.get('http://localhost:5002/turmas/1.5')
+        resposta = requests.get('http://localhost:5003/turmas/1.5')
         if resposta.status_code == 400:
             resp_retornada = resposta.json()
             assert {'mensagem': 'ID inserido para turma precisa ser um numero inteiro'} == resp_retornada
 
     def test_003_id_de_turma_menor_igual_que_zero(self):
-        resposta = requests.get('http://localhost:5002/turmas/-1')
+        resposta = requests.get('http://localhost:5003/turmas/-1')
         if resposta.status_code == 400:
             resp_retornada = resposta.json()                
             assert {'mensagem': 'ID de turma nao pode ser menor ou igual a que zero'} == resp_retornada
 
 
     def test_004_id_de_turma_inexistente(self):
-        resposta = requests.get('http://localhost:5002/turmas/500')
+        resposta = requests.get('http://localhost:5003/turmas/500')
         if resposta.status_code == 404:
             resp_retornada = resposta.json()
             assert {'mensagem': 'Turma nao encontrada/inexistente'} == resp_retornada  
 
 
 # -----------------------------------------------ALUNOS---------------------------------------------- #
+
     def test_001_alunos_retorna_lista(self):
-        resposta = requests.get('http://localhost:5002/alunos')
+        resposta = requests.get('http://localhost:5003/alunos')
 
         if resposta.status_code == 404:
             self.fail("voce nao definiu a pagina /alunos no seu server")
@@ -91,20 +117,20 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(type(obj_retornado),type([]))
 
     def test_002_id_de_aluno_não_int(self):
-        resposta = requests.get('http://localhost:5002/alunos/1.5')
+        resposta = requests.get('http://localhost:5003/alunos/1.5')
         if resposta.status_code == 400:
             resp_retornada = resposta.json()
             assert {'mensagem': 'ID inserido para aluno tem que ser um numero inteiro'} == resp_retornada
 
     def test_003_id_de_aluno_menor_igual_que_zero(self):
-        resposta = requests.get('http://localhost:5002/alunos/-1')
+        resposta = requests.get('http://localhost:5003/alunos/-1')
         if resposta.status_code == 400:
             resp_retornada = resposta.json()  
             assert {'mensagem': 'ID do aluno nao pode ser menor ou igual a que zero'} == resp_retornada
 
 
     def test_004_id_de_aluno_inexistente(self):
-        resposta = requests.get('http://localhost:5002/alunos/500')
+        resposta = requests.get('http://localhost:5003/alunos/500')
         if resposta.status_code == 404:
             resp_retornada = resposta.json()
             assert {'mensagem': 'Aluno(a) nao encontrado(a)/inexistente'} == resp_retornada
