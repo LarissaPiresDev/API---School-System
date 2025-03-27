91686720e8ee1f2123ffc4ae3f27df0da8ff9aba
Wender da Silva Santos <2401846@SALAS.AULAS>
2025-03-27 10:09:48 -0300

    :rocket: feat: adicionando última verificação para putTurma (caso a atualização de certo)

--- app.py
+++ app.py
@@ -128,11 +128,6 @@ def atualizar_professor(id):
         if salario < 1400:
             return jsonify({'mensagem': 'O novo valor para salário deve ser no mínimo 1400 e não pode ser negativo'}), 400
 
-
-
-
-
-
     professores = users['Professores']
     for index, professor in enumerate(professores):
         if professor['id'] == id:
@@ -253,16 +248,23 @@ def atualizar_turma(id):
     
     if id <= 0:
         return jsonify({'mensagem': 'ID de turma precisa ser maior que zero'}), 400
-    
-    turma_existe = False
-    for turma in users['Turmas']:
-        if id == turma['id']:
-            turma_existe = True
-            break
-        if not turma_existe:
-            return jsonify({'mensagem': 'Erro, ID de turma não encontrado'}), 404
         
     turma_atualizada = request.json
+
+    if 'ativo' in turma_atualizada:
+        if not isinstance(turma_atualizada['ativo'], bool):
+            return jsonify({'mensagem': 'O valor para a chave ativo precisa ser do tipo boolean'}), 400
+        
+
+    chaves_esperadas = {'descricao', 'professor_id', 'ativo'}
+    chaves_inseridas = set(turma_atualizada.keys())
+    chaves_invalidas = chaves_inseridas - chaves_esperadas
+    if chaves_invalidas:
+        return jsonify({
+            'mensagem': 'Chaves adicionais não necessárias, retire-as',
+            'chaves_invalidas': list(chaves_invalidas)
+        }), 400
+        
 
 
     if 'descricao' in turma_atualizada:
@@ -290,19 +292,16 @@ def atualizar_turma(id):
         if not prof_existe:
             return jsonify({'mensagem': 'Professor Id não encontrado, tente novamente '}), 404
     
-    if 'ativo' in turma_atualizada:
-        if not isinstance(turma_atualizada['ativo'], bool):
-            return jsonify({'mensagem': 'O valor para a chave ativo precisa ser do tipo boolean'}), 400
-        
 
-    chaves_esperadas = {'descricao', 'professor_id', 'ativo'}
-    chaves_inseridas = set(turma_atualizada.keys())
-    chaves_invalidas = chaves_inseridas - chaves_esperadas
-    if chaves_invalidas:
-        return jsonify({
-            'mensagem': 'Chaves adicionais não necessárias, retire-as',
-            'chaves_invalidas': list(chaves_invalidas)
-        }), 400
+    turmas = users['Turmas']
+    for index, turma in enumerate(turmas):
+        if turma['id'] == id:
+            turma_atualizada['id'] = turma['id']
+            users['Turmas'][index] = turma_atualizada
+            return jsonify({'mensagem': 'Turma atualizada com sucesso'}), 200
+    return jsonify({'mensagem': 'Erro, ID de turma não encontrado'}), 404
+
+    
     
         
     
