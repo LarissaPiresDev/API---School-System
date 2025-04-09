cc76248163d7345f8ecd32c5fdec23f625233082
Wender da Silva Santos <wenderman74@gmail.com>
2025-04-09 00:41:46 -0300

    chore: removendo código não utilizado e importando o módulo os

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, jsonify, request
-from users import users
+import os
 from config import app
 from alunos.alunos_routes import alunos_blueprint
 
 app.register_blueprint(alunos_blueprint)
+
 
 
 #--------------------------------------------PROFESSORES------------------------------------------- #
