2f92c63cb169c4643b6a2ecacbd203fae9e9c38a
Wender da Silva Santos <wenderman74@gmail.com>
2025-04-09 00:45:05 -0300

    :sparkles: feat: implementando a rota postAluno e geração de ID automática

--- alunos/alunos_model.py
+++ alunos/alunos_model.py
@@ -1,5 +1,6 @@
 users = {
-        "Alunos": [
+    
+    "Alunos": [
 
         {"id": 101, "nome": "Ana Heloisa", "idade": 13, "turma_id": 11, "data_nascimento": "10-11-2011", "nota_primeiro_semestre": 6.0, "nota_segundo_semestre": 8.0, "media_final": 7.00},
 
@@ -21,12 +22,23 @@ users = {
 
         {"id": 108, "nome": "Paulo Augusto", "idade": 12, "turma_id": 13, "data_nascimento": "28-11-2012", "nota_primeiro_semestre": 8.5, "nota_segundo_semestre": 7.5, "media_final": 8.0}
         
+    ],
+        
+    "Turmas": [
+        {"id": 11, "descricao": "7 Ano A", "professor_id": 1, "ativo": True},
+        {"id": 12, "descricao": "7 Ano B", "professor_id": 2, "ativo": True},
+        {"id": 13, "descricao": "7 Ano C", "professor_id": 3, "ativo": False}
+        
     ]
+        
+    
 
 }
 
 class AlunoNaoEncontrado(Exception):
     pass
+
+
     
 def listar_alunos():
     return users['Alunos']
@@ -37,6 +49,27 @@ def aluno_por_id(id):
         if aluno['id'] == id:
             return aluno
     raise AlunoNaoEncontrado
+
+def criar_aluno(novo_aluno):
+    id_novo = max(aluno['id'] for aluno in users['Alunos']) + 1
+    novo_aluno['id'] = id_novo
+    users['Alunos'].append(novo_aluno)
+    return novo_aluno
+
+
+class TurmaNaoEncontrada(Exception):
+    pass
+
+
+def achar_turma(turma_id):
+    turma_existe = False
+    for turma in users['Turmas']:
+        if turma['id'] == turma_id:
+            return True
+    raise TurmaNaoEncontrada
+
+
+
     
     
    
--- alunos/alunos_routes.py
+++ alunos/alunos_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from .alunos_model import AlunoNaoEncontrado, listar_alunos, aluno_por_id
+from .alunos_model import AlunoNaoEncontrado, listar_alunos, aluno_por_id, TurmaNaoEncontrada, achar_turma, criar_aluno
 
 alunos_blueprint = Blueprint('alunos', __name__)
 
@@ -23,3 +23,70 @@ def alunoPorId(id):
     
     except AlunoNaoEncontrado:
         return jsonify({'mensagem': 'Aluno(a) nao encontrado(a)/inexistente'}), 404
+    
+
+
+@alunos_blueprint.route('/alunos', methods=['POST'])
+def create_aluno():
+    novo_aluno = request.json
+
+    chaves_esperadas = {'nome', 'idade', 'turma_id', 'data_nascimento', 'nota_primeiro_semestre', 'nota_segundo_semestre', 'media_final'}
+
+    chaves_inseridas = set(novo_aluno.keys())
+
+    chaves_invalidas = chaves_inseridas - chaves_esperadas
+    if chaves_invalidas:
+        return jsonify({
+            'mensagem': 'Chaves adicionais não necessárias para a criação de aluno, retire-as',
+            'chaves_invalidas': list(chaves_invalidas)
+        }), 400
+    
+    if 'nome' not in novo_aluno or 'turma_id' not in novo_aluno:
+        return jsonify({'mensagem': 'Os campos nome, turma_id são OBRIGATÓRIOS'}), 400
+    
+    if not isinstance(novo_aluno['turma_id'], int) or not isinstance(novo_aluno['idade'], int):
+        return jsonify({'mensagem': 'O valor informado para as chaves idade e turma_id precisam ser INTEIROS'}), 400
+    
+    if novo_aluno['turma_id'] < 0:
+        return jsonify({'mensagem': 'O valor informado para a chave turma_id é INVÁLIDO (não pode ser negativo)'}), 400
+
+
+    try:
+        turmaexiste = achar_turma(novo_aluno['turma_id'])
+    except TurmaNaoEncontrada:
+        return jsonify({'mensagem': 'Id de turma não encontrada'}), 404
+    
+    if not (isinstance(novo_aluno['nome'], str)):
+        return jsonify({'mensagem': 'Chave nome precisa ser do tipo string'}), 400
+    
+    if novo_aluno['idade'] <= 0:
+        return jsonify({'mensagem': 'O valor informado na chave idade não pode ser negativo ou igual a zero'}),400
+    
+    if 'data_nascimento' in novo_aluno:
+        if not isinstance(novo_aluno['data_nascimento'], str) or not novo_aluno['data_nascimento'].strip():
+            return jsonify({'mensagem': 'Data de Nascimento precisa ser uma string dd-mm-aaaa e não pode estar vazia'}), 400
+
+    
+    if 'nota_primeiro_semestre' in novo_aluno or 'nota_segundo_semestre' in novo_aluno or 'media_final' in novo_aluno:
+        
+        if not isinstance(novo_aluno['nota_primeiro_semestre'], (int, float)) or not isinstance(novo_aluno['nota_segundo_semestre'], (int, float)) or not isinstance(novo_aluno['media_final'], (int, float)):
+            return jsonify({'mensagem': 'Os valores para as notas de primeiro, segundo, semestre, precisao ser do tipo INTEIRO ou FLOAT'}), 400
+
+        if novo_aluno['nota_primeiro_semestre'] < 0 or novo_aluno['nota_segundo_semestre'] < 0 or novo_aluno['media_final'] < 0:
+            return jsonify({'mensagem' : 'As notas e a media precisam receber um valor inteiro ou float'}), 400
+    
+    
+
+
+
+    if 'nota_primeiro_semestre' not in novo_aluno:
+        novo_aluno['nota_primeiro_semestre'] = 0.0
+    if 'nota_segundo_semestre' not in novo_aluno:
+        novo_aluno['nota_segundo_semestre'] = 0.0
+    if 'media_final' not in novo_aluno:
+        novo_aluno['media_final'] = 0.0
+    
+    
+    novo_aluno_criado = criar_aluno(novo_aluno)
+
+    return jsonify(novo_aluno_criado), 201
