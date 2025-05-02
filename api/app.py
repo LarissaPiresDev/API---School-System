605b32400cbbeacbb55a37e2c444b200f1e8188c
Wender da Silva Santos <wenderman74@gmail.com>
2025-05-02 19:13:36 -0300

    :rocket: feat: atualizando as configurações para rodarmos nossa aplicação com o sqlite

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
 import os
-from config import app
+from config import app,db
 from alunos.alunos_routes import alunos_blueprint
 from professor.professores_routes import professor_blueprint
 from turma.turmas_routes import turma_blueprint
@@ -7,6 +7,9 @@ from turma.turmas_routes import turma_blueprint
 app.register_blueprint(alunos_blueprint)
 app.register_blueprint(professor_blueprint)
 app.register_blueprint(turma_blueprint)
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(host=app.config["HOST"], port = app.config['PORT'],debug=app.config['DEBUG']) 
