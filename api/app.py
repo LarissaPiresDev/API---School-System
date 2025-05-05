f53e40b46dab89744165dfb88561806c83f1f5a7
Wender da Silva Santos <wenderman74@gmail.com>
2025-05-04 22:40:54 -0300

    :rocket: feat: adicionando swagger no nosso app.py para rodarmos ele no render também

--- api/app.py
+++ api/app.py
@@ -3,10 +3,13 @@ from config import app,db
 from alunos.alunos_routes import alunos_blueprint
 from professor.professores_routes import professor_blueprint
 from turma.turmas_routes import turma_blueprint
+from swagger.swagger_config import configure_swagger
 
 app.register_blueprint(alunos_blueprint)
 app.register_blueprint(professor_blueprint)
 app.register_blueprint(turma_blueprint)
+
+configure_swagger(app)
 
 with app.app_context():
     db.create_all()
